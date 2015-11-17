2942f74751df4abef6dc18ff783d1117c8c29c9d
throck95 <tylerthrock95@gmail.com>
2015-11-17 13:42:02 -0500

    first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,20 +10,20 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-/*
+
 
 
 Route::get("products","ProductController@products");
 Route::get("products/{id}","ProductController@getProductById");
 Route::get("/","ProductController@welcome");
 
-*/
+
 
 /*Route::resource("songs","MySongsController",["names"=>["index"=>"songs_path","show"=>"song_path"],"except"=>"destroy"]);
 Route::controllers([
     'auth' => 'Auth\AuthController',
     'password' => 'Auth\PasswordController',
 ]);*/
-
+/*
 Route::get("/","ImageController@welcome");
-Route::resource("image","ImageController",["names"=>["index"=>"images_path","show"=>"image_path","create"=>"image_create","store"=>"image_store","update"=>"image_update","edit"=>"image_edit","destroy"=>"image_destroy"],"except"=>["destroy","edit"]]);
+Route::resource("image","ImageController",["names"=>["index"=>"images_path","show"=>"image_path","create"=>"image_create","store"=>"image_store","update"=>"image_update","edit"=>"image_edit","destroy"=>"image_destroy"],"except"=>["destroy","edit"]]);*/
